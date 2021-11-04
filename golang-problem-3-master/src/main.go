f385d9d5517defc7c59dabc9f475d65e35b8ed9e
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2021-11-04 23:59:02 +0330

    [fix] some changes on problem 3

--- golang-problem-3-master/src/main.go
+++ golang-problem-3-master/src/main.go
@@ -20,6 +20,7 @@ var usersList []BaleImpl
 var usernameChecker = regexp.MustCompile(`^[a-zA-Z0-9_]*$`).MatchString 
 var chatList []BaleChat
 var chatMessageList []BaleMessages
+var likedMessages []BaleLikedMessages
 
 type BaleImpl struct {
 	username string
@@ -49,6 +50,11 @@ func NewBaleImpl() *BaleImpl {
 
 type MessageError struct {
     message string
+}
+
+type BaleLikedMessages struct {
+	userId int;
+	messageId int;
 }
 
 func (e *MessageError) Error() string {
@@ -124,4 +130,81 @@ func (bale BaleImpl) SendMessage(userId, chatId int, text string) (int, error) {
 	} else {
 		return 0, &MessageError{message: "user could not send message"}
 	}
+}
+
+func (bale BaleImpl) SendLike(userId, messageId int) error {
+	validMessage := false
+	//check if message exist
+	for i := 0; i <len(chatMessageList); i++ {
+		if messageId == chatMessageList[i].messageId {
+			validMessage = true
+		}
+	}
+
+	if !validMessage {
+		return &MessageError{message: "message not found"}
+	}
+	
+	//check if this message liked before or not
+	for i := 0; i < len(likedMessages); i++ {
+		if likedMessages[i].messageId == messageId && likedMessages[i].userId == userId {
+			return &MessageError{message: "this user has liked this message before"}
+			break
+		}
+	}
+
+	//succesfull process
+	baleLikeMessage := new(BaleLikedMessages)
+	baleLikeMessage.userId = userId
+	baleLikeMessage.messageId = messageId
+	likedMessages = append(likedMessages, *baleLikeMessage)
+	return nil
+}
+
+func (bale BaleImpl) GetNumberOfLikes(messageId int) (int, error) {
+	var i = 0
+	var counter = 0
+	for i = 0; i < len(likedMessages); i++ {
+		if likedMessages[i].messageId == messageId {
+			counter++
+		}
+	}
+
+	return counter, nil
+}
+
+func (bale BaleImpl) SetChatAdmin(chatId, userId int) error {
+	for i := 0; i < len(chatList); i++ {
+		if chatId == chatList[i].chatId {
+			for j := 0; j < len(chatList[i].adminIds); j++ {
+				if userId == chatList[i].adminIds[j] {
+					return &MessageError{message: "user is already admin"}
+				}
+			}
+			chatList[i].adminIds = append(chatList[i].adminIds, userId)
+			return nil
+		}
+	}
+
+	return &MessageError{message: "This chat doesn't exist."}
+}
+
+func GetLastMessage(chatId int) (string, int, error) {
+	for i := len(chatMessageList) - 1; i >= 0; i-- {
+		if chatId == chatMessageList[i].receiverChatId {
+			return chatMessageList[i].message, chatMessageList[i].messageId, nil
+		}
+	}
+
+	return "", 0, &MessageError{message: "This chat doesn't exist."}
+}
+
+func GetLastUserMessage(userId int) (string ,int, error) {
+	for i := len(chatMessageList) - 1; i >= 0; i-- {
+		if userId == chatMessageList[i].senderId {
+			return chatMessageList[i].message, chatMessageList[i].messageId, nil
+		}
+	}
+
+	return "", 0, &MessageError{message: "This chat doesn't exist."}
 }
