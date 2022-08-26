5c825e8c3a363ff294bedc1a2a8e488227dbcce1
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2022-08-26 15:36:06 +0430

    [feat] add prime number decomposition rpc method to proto and gRPC files

--- grpc-exercises/first/proto/api/calculator_grpc.pb.go
+++ grpc-exercises/first/proto/api/calculator_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type CalculatorServiceClient interface {
 	Sum(ctx context.Context, in *SumRequest, opts ...grpc.CallOption) (*SumResponse, error)
+	PrimeNumberDecomposition(ctx context.Context, in *PrimeNumberDecompositionRequest, opts ...grpc.CallOption) (*PrimeNumberDecompositionResponse, error)
 }
 
 type calculatorServiceClient struct {
@@ -42,11 +43,21 @@ func (c *calculatorServiceClient) Sum(ctx context.Context, in *SumRequest, opts 
 	return out, nil
 }
 
+func (c *calculatorServiceClient) PrimeNumberDecomposition(ctx context.Context, in *PrimeNumberDecompositionRequest, opts ...grpc.CallOption) (*PrimeNumberDecompositionResponse, error) {
+	out := new(PrimeNumberDecompositionResponse)
+	err := c.cc.Invoke(ctx, "/calculator.CalculatorService/PrimeNumberDecomposition", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CalculatorServiceServer is the server API for CalculatorService service.
 // All implementations must embed UnimplementedCalculatorServiceServer
 // for forward compatibility
 type CalculatorServiceServer interface {
 	Sum(context.Context, *SumRequest) (*SumResponse, error)
+	PrimeNumberDecomposition(context.Context, *PrimeNumberDecompositionRequest) (*PrimeNumberDecompositionResponse, error)
 	mustEmbedUnimplementedCalculatorServiceServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedCalculatorServiceServer struct {
 
 func (UnimplementedCalculatorServiceServer) Sum(context.Context, *SumRequest) (*SumResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Sum not implemented")
+}
+func (UnimplementedCalculatorServiceServer) PrimeNumberDecomposition(context.Context, *PrimeNumberDecompositionRequest) (*PrimeNumberDecompositionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method PrimeNumberDecomposition not implemented")
 }
 func (UnimplementedCalculatorServiceServer) mustEmbedUnimplementedCalculatorServiceServer() {}
 
@@ -88,6 +102,24 @@ func _CalculatorService_Sum_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
+func _CalculatorService_PrimeNumberDecomposition_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(PrimeNumberDecompositionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CalculatorServiceServer).PrimeNumberDecomposition(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/calculator.CalculatorService/PrimeNumberDecomposition",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CalculatorServiceServer).PrimeNumberDecomposition(ctx, req.(*PrimeNumberDecompositionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // CalculatorService_ServiceDesc is the grpc.ServiceDesc for CalculatorService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var CalculatorService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Sum",
 			Handler:    _CalculatorService_Sum_Handler,
+		},
+		{
+			MethodName: "PrimeNumberDecomposition",
+			Handler:    _CalculatorService_PrimeNumberDecomposition_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
