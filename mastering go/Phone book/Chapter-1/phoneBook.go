8450e66b294f403183226e436f55bdf8ac246278
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2024-06-05 19:32:32 +0330

    finish exercise of chapter two

--- mastering go/Phone book/Chapter-1/phoneBook.go
+++ mastering go/Phone book/Chapter-1/phoneBook.go
@@ -3,7 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"os"
+	"strconv"
 )
 
 type Entry struct {
@@ -12,27 +14,61 @@ type Entry struct {
 	Telephone string
 }
 
-var data = []Entry{}
+const maxStringLength = 7
+const phoneLength = 11
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func search(key string) (Entry, error) {
+func search(data []Entry, key string) (Entry, error) {
 	for _, v := range data {
 		if v.Surname == key {
 			return v, nil
 		}
 	}
-	return Entry{}, errors.New("Not found")
+	return Entry{}, errors.New("not found")
 }
 
-func list() ([]Entry, error) {
+func list(data []Entry) ([]Entry, error) {
 	result := []Entry{}
-	for _, v := range data {
-		result = append(result, v)
-	}
+	result = append(result, data...)
 
 	return result, nil
 }
 
+func generateRandomString(count int32) string {
+	result := ""
+	for i := 0; i < int(count); i++ {
+		randomInteger := rand.Intn(len(charset))
+		result += string(charset[randomInteger])
+	}
+
+	return result
+}
+
+func generateRandomPhone(phoneLength int32) string {
+	result := ""
+	for i := 0; i < int(phoneLength); i++ {
+		randomInteger := rand.Intn(10)
+		result += strconv.Itoa(randomInteger)
+	}
+
+	return result
+}
+
+func manipulateData(dataCount int32, data []Entry) []Entry {
+	for i := 0; i < int(dataCount); i++ {
+		data = append(data, Entry{
+			Name:      generateRandomString(maxStringLength),
+			Surname:   generateRandomString(maxStringLength),
+			Telephone: generateRandomPhone(phoneLength),
+		})
+	}
+
+	return data
+}
+
 func main() {
+	var data = []Entry{}
+
 	arguments := os.Args
 	if len(arguments) == 1 {
 		fmt.Println("Please enter required arguments!!")
@@ -43,6 +79,8 @@ func main() {
 	data = append(data, Entry{"Mary", "Doe", "2109416871"})
 	data = append(data, Entry{"John", "Black", "2109416123"})
 
+	manipulatedData := manipulateData(100, data)
+
 	switch arguments[1] {
 	case "search":
 		if len(arguments) != 3 {
@@ -50,7 +88,7 @@ func main() {
 			return
 		}
 
-		result, err := search(arguments[2])
+		result, err := search(manipulatedData, arguments[2])
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -59,7 +97,7 @@ func main() {
 		fmt.Println(result)
 
 	case "list":
-		userList, err := list()
+		userList, err := list(manipulatedData)
 		if err != nil {
 			fmt.Println(err)
 			return
